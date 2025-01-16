0a65b5f1aa7475f1d9bcfd3d9e7bb97b11358f35
Mahika Bhan <bhan.mahika813@gmail.com>
2025-01-15 22:56:22 -0800

    f

---
+++ Node.h
@@ -0,0 +1,24 @@
+// from emma bendersky
+
+#ifndef NODE_H
+#define NODE_H
+#include <iostream>
+#include "Student.h"
+
+using namespace std;
+
+class Node
+{
+ public:
+  Node(Student*);
+  ~Node();
+  Node* getNext();
+  Student* getStudent();
+  void setNext(Node*);
+  
+ private:
+  Student* student;
+  Node* next;
+};
+
+#endif
