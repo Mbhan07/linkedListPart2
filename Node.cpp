502fc1f7af2c3aa1f6bf51e676ff3ffa7d19ddd9
Mahika Bhan <bhan.mahika813@gmail.com>
2025-01-16 23:00:49 -0800

    comments

--- Node.cpp
+++ Node.cpp
@@ -1,8 +1,17 @@
-// from emma bendersky
+
+/* This is one of the files that I used from emma bendersky. The Node.cpp file allows for implementation of the Node class
+
+   Mahika Bhan, 01/16/2025
+*/
 
 #include "Node.h"
 
 using namespace std;
+
+
+//constructor: initializes node w/ pointer to student obj
+//set Node pointer to null
+
 
 Node::Node(Student* newStudent)
 {
@@ -10,22 +19,27 @@ Node::Node(Student* newStudent)
   next = NULL;
 }
 
+//destructor (to free up mempory)
 Node::~Node()
 {
   delete student;
   next = NULL;
 }
 
+//returns pointer to next node in the LL
 Node* Node::getNext()
 {
   return next;
 }
 
+//set pointer to new node in LL
 void Node::setNext(Node* newNext)
 {
   next = newNext;
 }
 
+
+//return pointer to a student obj in node
 Student* Node::getStudent()
 {
   return student;
