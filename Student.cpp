a9fdd813844d3af152b2588f3a86c23ae130eb06
Mahika Bhan <bhan.mahika813@gmail.com>
2025-01-16 23:02:56 -0800

    comments

--- Student.cpp
+++ Student.cpp
@@ -1,7 +1,9 @@
 
 /*
-In this file, it contains the functions for the student class
- */
+In this file, it contains the functions for the student class. Allows for managing of information of a student, including first name, last name, id, GPA
+
+Mahika Bhan 01/16/2025
+*/
 
 
 //this is student.cpp file
@@ -18,11 +20,17 @@ using namespace std;
 //create student using inputted information
 
 Student::Student(char * firstNameInput, char * lastNameInput, int inputid, float inputgpa){
+
+  //allocate memory for first and last name
   firstname = new char[200];
   lastname = new char [200];
 
+  //then copy input into allocated memoery
+
   strcpy(firstname, firstNameInput);
   strcpy(lastname, lastNameInput);
+
+  //assign ID/GPA values
 
   id = inputid;
   gpa = inputgpa;
@@ -37,16 +45,21 @@ char * Student::getFirstName(){
   return firstname;
 }
 
+//return last name
 
 char * Student::getLastName(){
 
   return lastname;
 }
 
+//return ID
+
 int Student::getID() {
 
   return id;
 }
+
+//return GPA
 
 float Student::getGPA(){
 
