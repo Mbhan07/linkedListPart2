1e7946f1ba8ff0c7e81182a873c322d17228534f
Mahika Bhan <bhan.mahika813@gmail.com>
2025-01-16 23:02:35 -0800

    comments

--- Node.h
+++ Node.h
@@ -1,5 +1,10 @@
-// from emma bendersky
+/*
+  This is the second file I recieved from emma bendersky. This is the Node.h class which defines the Node class, which representa a node in a LL. Each node contains a pointer to a Student object/pointer to next node.
 
+  Mahika Bhan, 1/16/2025
+ */
+
+//header guard
 #ifndef NODE_H
 #define NODE_H
 #include <iostream>
@@ -7,18 +12,19 @@
 
 using namespace std;
 
-class Node
+class Node // create Node class
 {
  public:
-  Node(Student*);
-  ~Node();
-  Node* getNext();
-  Student* getStudent();
-  void setNext(Node*);
+  Node(Student*); //constructor
+  ~Node(); // destructor
+  Node* getNext(); //getNext() method
+  Student* getStudent(); //getStudent() method
+  void setNext(Node*); //setNext() method
   
  private:
-  Student* student;
-  Node* next;
+  Student* student; //student object
+  Node* next; //pointer to next node inLL
 };
 
 #endif
+//end of header guard
